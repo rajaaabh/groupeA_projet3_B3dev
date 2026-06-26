69b989932490f4a7a8127ade420cb81955683098
chrys <steph2civ@gmail.com>
2026-06-26 16:06:01 +0200

    fix: ajout  du Render URL dans le  CORS

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => array_filter([
+        'http://localhost:5173',
+        env('FRONTEND_URL'),
+    ]),
 
     'allowed_origins_patterns' => [],
 
