0f5d07effb8c0d942a2dc920135bb8812bbd518a
cheyssd <steph2civ@gmail.com>
2026-06-28 15:10:04 +0200

    Add dynamic FRONTEND_URL to allowed_origins

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+    'http://localhost:5173',
+    env('FRONTEND_URL', 'http://localhost:5173'),
+],
 
     'allowed_origins_patterns' => [],
 
